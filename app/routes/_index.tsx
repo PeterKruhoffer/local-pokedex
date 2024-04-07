2a08dbdb0a25076c0de4729b38477b9335e7e169
peterkruhoffer <peter.kruhoffer@gmail.com>
2024-04-07 16:57:28 +0200

    init and got basic localforage working

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import localforage from "localforage";
+
+type Pokemon = {
+  name: string;
+  types: string[];
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +14,55 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const localPokedexStore = localforage.createInstance({
+  name: "localPokedexStore",
+  storeName: "pokedex",
+});
+
+export const clientLoader = async () => {
+  //localPokedexStore.removeItem("dex");
+  const pokemons: Pokemon[] = [];
+  await localPokedexStore.iterate((value: Pokemon, key) => {
+    pokemons.push({
+      name: key,
+      types: [...value.types],
+    });
+  });
+
+  return pokemons;
+};
+
+export function HydrateFallback() {
+  return <p>Loading...</p>;
+}
+
 export default function Index() {
+  const pokemons = useLoaderData<typeof clientLoader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+    <div className="min-h-screen bg-slate-900 p-20">
+      <h1 className="text-3xl text-white">Welcome to Remix</h1>
+      <ul className="border border-red-400/30 p-20">
+        <li className="text-2xl text-white">Hello world</li>
+        {pokemons.map((pokemon, i) => (
+          <li key={i} className="text-white text-2xl flex gap-2">
+            {pokemon.name}
+            {pokemon.types.map((t, index) => (
+              <span key={index}>{t}</span>
+            ))}
+          </li>
+        ))}
       </ul>
+      <button
+        onClick={() =>
+          localPokedexStore.setItem("bayleaf", {
+            name: "bayleaf",
+            types: ["grass"],
+          })
+        }
+        className="text-white p-2 border-red-400/30"
+      >
+        Save item
+      </button>
     </div>
   );
 }
