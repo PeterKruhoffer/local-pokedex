88e870ec60920d217dd8b5fdf33b9579dfb43f07
peterkruhoffer <peter.kruhoffer@gmail.com>
2024-04-12 20:49:00 +0200

    Now saves all pokemon to local db

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,8 +3,9 @@ import { useLoaderData } from "@remix-run/react";
 import localforage from "localforage";
 
 type Pokemon = {
-  name: string;
-  types: string[];
+  results: {
+    name: string;
+  }[];
 };
 
 export const meta: MetaFunction = () => {
@@ -14,55 +15,51 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-const localPokedexStore = localforage.createInstance({
-  name: "localPokedexStore",
-  storeName: "pokedex",
-});
-
-export const clientLoader = async () => {
-  //localPokedexStore.removeItem("dex");
-  const pokemons: Pokemon[] = [];
-  await localPokedexStore.iterate((value: Pokemon, key) => {
-    pokemons.push({
-      name: key,
-      types: [...value.types],
-    });
-  });
-
-  return pokemons;
-};
+export async function clientLoader() {
+  await getPokemons();
+  return memory;
+}
 
 export function HydrateFallback() {
   return <p>Loading...</p>;
 }
 
+let memory: Pokemon | null;
+const getPokemons = async () => {
+  const pokemons: Pokemon | null = await localforage.getItem("pokedex");
+  if (pokemons) {
+    memory = pokemons;
+    return;
+  }
+  const headers = new Headers();
+  headers.append("Cache-Control", "60 * 60 * 24");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151", {
+    headers: headers,
+  });
+  const pokemon = await response.json();
+  localforage.setItem("pokedex", pokemon);
+  memory = pokemon;
+};
+
 export default function Index() {
   const pokemons = useLoaderData<typeof clientLoader>();
+  if (!pokemons) {
+    return <span>no pokemons</span>;
+  }
   return (
-    <div className="min-h-screen bg-slate-900 p-20">
+    <div className="min-h-screen bg-slate-900 p-40">
       <h1 className="text-3xl text-white">Welcome to Remix</h1>
       <ul className="border border-red-400/30 p-20">
-        <li className="text-2xl text-white">Hello world</li>
-        {pokemons.map((pokemon, i) => (
-          <li key={i} className="text-white text-2xl flex gap-2">
-            {pokemon.name}
-            {pokemon.types.map((t, index) => (
-              <span key={index}>{t}</span>
-            ))}
-          </li>
-        ))}
+        <div className="grid grid-flow-row grid-cols-3 gap-8">
+          {pokemons.results.map(
+            (pokemon: Pokemon["results"][number], i: number) => (
+              <li key={i} className="text-white text-2xl text-center">
+                {pokemon.name.toUpperCase()}
+              </li>
+            ),
+          )}
+        </div>
       </ul>
-      <button
-        onClick={() =>
-          localPokedexStore.setItem("bayleaf", {
-            name: "bayleaf",
-            types: ["grass"],
-          })
-        }
-        className="text-white p-2 border-red-400/30"
-      >
-        Save item
-      </button>
     </div>
   );
 }
